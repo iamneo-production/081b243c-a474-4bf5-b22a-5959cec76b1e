82097333f17a9d4cb244e0bc56d8c99285150176
denam98 <dchamath5@gmail.com>
2023-08-12 13:52:15 +0000

    fix

--- routez-bmi-calculator/src/components/button/button.tsx
+++ routez-bmi-calculator/src/components/button/button.tsx
@@ -2,17 +2,6 @@
 import Button from '@mui/material/Button';
 import React from 'react'
 import './button.css'
-import { styled } from '@mui/material/styles';
-import { purple } from '@mui/material/colors';
-import { ButtonProps } from '@mui/base';
-
-const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
-  color: theme.palette.getContrastText(purple[500]),
-  backgroundColor: purple[500],
-  '&:hover': {
-    backgroundColor: purple[700],
-  },
-}));
 
 export const Custom_button = (props: any) => {
   let color;
