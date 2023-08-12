5a133108feb44dc2abfa68df01fdb06e23006011
denam98 <dchamath5@gmail.com>
2023-08-12 10:48:41 +0000

    landing page done

--- routez-bmi-calculator/src/components/button/button.tsx
+++ routez-bmi-calculator/src/components/button/button.tsx
@@ -1,11 +1,46 @@
-import { Button } from '@mui/material'
+
+import Button from '@mui/material/Button';
 import React from 'react'
-import 'button.css'
+import './button.css'
+import { styled } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+import { ButtonProps } from '@mui/base';
 
+const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
+  color: theme.palette.getContrastText(purple[500]),
+  backgroundColor: purple[500],
+  '&:hover': {
+    backgroundColor: purple[700],
+  },
+}));
 
-export const Custom_button = (props:any) => {
+export const Custom_button = (props: any) => {
+  let color = '#2AFF7F';
+  let backgroundColor = '#2AFF7F';
+  let borderColor = '#2AFF7F';
+  if (props.variant == 'contained') {
+    color = 'white';
+    backgroundColor = '#2AFF7F';
+    borderColor = '#2AFF7F';
+  } else {
+    color = '#2AFF7F';
+    backgroundColor = '#000033'
+    borderColor = '#2AFF7F';
+  }
   return (
-    <Button size={props.size} variant={props.variant} className={props.variant}>{props.name}</Button>
+    <Button sx={{ 
+      color: { color }, 
+      backgroundColor: { backgroundColor }, 
+      borderColor: { borderColor }, 
+      fontWeight: '600', 
+      margin: '5px', 
+      marginBottom: '10px' 
+    }} 
+    size={props.size} 
+    variant={props.variant} 
+    className={props.variant}>
+      {props.name}
+    </Button>
   )
 }
 
