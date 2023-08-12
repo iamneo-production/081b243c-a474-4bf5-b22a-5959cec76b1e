5c6244d0f708f1bcaf5d061e157011b785166505
nadith98 <ggnadith2@gmail.com>
2023-08-12 09:00:44 +0000

    gender

--- routez-bmi-calculator/src/routes/index.ts
+++ routez-bmi-calculator/src/routes/index.ts
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
-
-import Login from "../pages/login";
+// import {Login} from "../pages/login";
+import { Login } from "../pages/login/login";
+import { Gender } from "../pages/gender/gender";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     Component: Login,
+  },
+  {
+    path: "/gender",
+    Component: Gender,
   },
 ]);
 
