648d57f75b515360aeba5563510efb89ffd5e818
hasunnilupul <hasunnilupul16@gmail.com>
2023-08-13 06:15:20 +0000

    Refactored button styles

--- routez-bmi-calculator/src/components/button/button.tsx
+++ routez-bmi-calculator/src/components/button/button.tsx
@@ -1,5 +1,6 @@
 
 import Button from '@mui/material/Button';
+import { width } from '@mui/system';
 import React from 'react'
 import './button.css'
 
@@ -7,27 +8,39 @@ export const CustomButton = (props: any) => {
   let color;
   let backgroundColor;
   let borderColor;
+  let hoverBackgroundColor;
+  let hoverBorderColor;
   if (props.variant === 'contained') {
     color = 'white';
     backgroundColor = '#2AFF7F';
+    hoverBackgroundColor='#2AFF6FC9';
     borderColor = '#2AFF7F';
   } else {
     color = '#2AFF7F';
-    backgroundColor = '#000033'
+    backgroundColor = 'transparent'
+    hoverBackgroundColor='#2AFF6F0F';
     borderColor = '#2AFF7F';
+    hoverBorderColor = '#2AFF6FC9';
   }
   return (
     <Button sx={{ 
       color: { color }, 
-      backgroundColor: { backgroundColor }, 
-      borderColor: { borderColor }, 
+      backgroundColor: { backgroundColor },
+      borderColor: { borderColor },
+      '&:hover': {
+        backgroundColor: hoverBackgroundColor,
+        borderColor: hoverBorderColor
+      }, 
       fontWeight: '600', 
       margin: '5px', 
-      marginBottom: '10px' 
+      marginBottom: '10px',
+      width: '90%'
     }} 
     size={props.size} 
     variant={props.variant} 
-    className={props.variant}>
+    className={props.className}
+    onClick={props?.onClick}
+    href={props?.href}>
       {props.name}
     </Button>
   )
