290a9bb083acf572ec31e31b0b03ce29c5d07ef3
BuddhikaDilupul <buddhikadilupulr@gmail.com>
2023-08-13 14:31:32 +0000

    bmi display page added

---
+++ routez-bmi-calculator/src/pages/bmi-display/displayCard.tsx
@@ -0,0 +1,62 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { useEffect, useState } from 'react';
+import { CustomButton } from '../../components/button/button';
+import { useNavigate } from 'react-router-dom';
+import '../bmi-display/bmi-display.css'; 
+
+const DisplayBMI = () => {
+  const [bmi, setBmi] = useState("22");
+  const [bmiText, setBmiText] = useState("Over");
+  const textColor = bmiText === "Avarage" ? "bmi-text-green" : "bmi-text-red";
+  const navigate = useNavigate()
+  useEffect(() => {
+
+    const apiUrl = '';
+    fetch(apiUrl)
+      .then(response => {
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setBmi(data.bmi);
+        setBmiText(data.bmi);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+
+  }, [])
+
+  return (
+    <>
+      <div className="container">
+      <h2 className="header">Your BMI</h2>
+      <div className="container-card">
+        <Card className="card" style={{backgroundColor:"#292D36"}}>
+          <CardContent>
+            <Typography className="bmi-number" color="#FFF">
+              <h1>{bmi}</h1>
+            </Typography>
+            <Typography className={textColor}>
+              {bmiText}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="button-container  full-width">
+        <CustomButton
+          name="< Go Back"
+          size="small"
+          onClick={() => { navigate("/") }}
+        />
+      </div>
+    </div>
+    </>
+  );
+}
+export default DisplayBMI
--- routez-bmi-calculator/src/routes/routes.ts
+++ routez-bmi-calculator/src/routes/routes.ts
@@ -3,6 +3,7 @@ import { Login } from "../pages/login/login";
 import { Landing } from "../pages/landing/landing";
 import { Register } from "../pages/register/register";
 import { BMICalculator } from "../pages/bmi-calculator/bmi-calculator";
+import DisplayBMI from "../pages/bmi-display/displayCard";
 
 const routes = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const routes = createBrowserRouter([
   {
     path: "/calculator",
     Component: BMICalculator,
+  },
+  {
+    path: "/bmi",
+    Component: DisplayBMI,
   },
 ]);
 
